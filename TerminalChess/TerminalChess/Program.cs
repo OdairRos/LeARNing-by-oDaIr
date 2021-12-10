1bf7f7be3e223766d6ff3265051a5c815a2112c4
Odair <odairtopper@gmail.com>
2021-12-10 11:54:13 -0300

    adicionando arquivos

--- TerminalChess/TerminalChess/Program.cs
+++ TerminalChess/TerminalChess/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-
+using Tabuleiro;
 namespace TerminalChess
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
